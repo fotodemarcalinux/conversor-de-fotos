f36f0200d8109ec2dab4be23620b710b7cb37f23
fotodemarcalinux <fotodemarca+linux@gmail.com>
2024-06-22 01:51:54 -0300

    Corrigir função process-images para retornar arquivo zip corretamente com imagens convertidas

--- netlify/functions/process-images.js
+++ netlify/functions/process-images.js
@@ -3,6 +3,7 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 const archiver = require('archiver');
+const { Handler } = require('@netlify/functions');
 
 exports.handler = async (event) => {
   return new Promise((resolve, reject) => {
